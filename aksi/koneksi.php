e6e24e5360b2871d8d9aabc009d8aad077da1489
Agung Dwi Sahputra <agungdwisahputra@gmail.com>
2025-09-28 14:47:07 +0700

    Change koneksi to prod

--- aksi/koneksi.php
+++ aksi/koneksi.php
@@ -1,8 +1,12 @@
 <?php 
-	$servername = "localhost";
-	$username = "root";
-	$password = "toor";
-	$db = "pos-senimankoding-v7";
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "toor";
+	// $db = "pos-senimankoding-v7";
+	$servername = "anzio-db.id.domainesia.com";
+	$username = "dekreat1_pos";
+	$password = "_4qZGKgNO.#B&qvC";
+	$db = "dekreat1_stagging-pos";
 
 	$conn = new mysqli($servername, $username, $password, $db);
 	date_default_timezone_set('Asia/Jakarta');
